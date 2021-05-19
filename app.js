19f913e2d010442f8057cc883fe12816b7898fe0
deepthy <deepthyp1991@gmail.com>
2021-05-19 15:53:06 -0400

    Add user and index routes

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const reviewsRouter = require('./routes/reviews');
 const postsRouter = require('./routes/posts')
 
@@ -23,8 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
-app.use('/posts/:id/reviews', postsRouter);
-app.use('/users', usersRouter);
+app.use('/posts/:id/reviews', reviewsRouter);
+
 
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,53 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'surf shop - Home' });
 });
 
+/* GET /register. */
+router.get('/', (req, res, next) => {
+  res.send('GET /register')
+});
+
+/* POST /register. */
+router.post('/', (req, res, next) => {
+  res.send('POST /register')
+});
+
+/* GET /login. */
+router.get('/login', (req, res, next) => {
+  res.send('GET /login')
+});
+
+/* POST /login. */
+router.post('/login', (req, res, next) => {
+  res.send('POST /login')
+});
+
+/* GET /profile. */
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile')
+});
+
+/* PUT /profile/:user_id. */
+router.get('/profile/:user_id', (req, res, next) => {
+  res.send('PUT /profile/:user_id')
+});
+
+/* GET /forget-password */
+router.get('/forget-password', (req, res, next) => {
+  res.send('GET /forget-password')
+});
+
+/* PUT /forget-password */
+router.get('/forget-password', (req, res, next) => {
+  res.send('PUT /forget-password')
+});
+
+/* GET /reset-password */
+router.get('/reset-password', (req, res, next) => {
+  res.send('GET /reset-password')
+});
+
+/* PUT /reset-password */
+router.put('/reset-password', (req, res, next) => {
+  res.send('PUT /reset-password')
+});
 module.exports = router;
