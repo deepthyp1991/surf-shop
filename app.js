ffe142111ad7be8cf55b364114d056e22254c30a
deepthy <deepthyp1991@gmail.com>
2021-05-31 17:03:36 -0400

    Update all the POST routes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const passport = require('passport');
 const User = require('./models/user');
 const session = require('express-session')
 const mongoose = require('mongoose')
-
+const methodOverride = require('method-override')
 
 //require routes
 const indexRouter = require('./routes/index');
@@ -35,9 +35,11 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'))
+
 
 
 //Configure passport and sessions
--- routes/posts.js
+++ routes/posts.js
@@ -1,39 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const {asyncErrorHandler} = require('../middleware')
+const {
+  postIndex, 
+  postNew, 
+  postCreate, 
+  postShow, 
+  postEdit,
+  postUpdate,
+  postDestroy
+} = require('../controllers/posts')
 
 /*  Get post index/posts */
-router.get('/', (req, res, next) => {
-  res.send('INDEX /posts');
-});
+router.get('/',asyncErrorHandler(postIndex) );
 
 /*  Get post new /posts/new */
-router.get('/new', (req, res, next) => {
-  res.send('NEW /posts/new');
-});
+router.get('/new', postNew);
 
 /*  POST post create/posts */
-router.post('/', (req, res, next) => {
-  res.send('CREATE /posts');
-});
+router.post('/', asyncErrorHandler(postCreate));
 
 /*  Get post show /posts/:id */
-router.get('/:id', (req, res, next) => {
-  res.send('SHOW /posts/:id');
-}); 
+router.get('/:id', asyncErrorHandler(postShow)); 
 
 /*  Get post edit /posts/:id/edit  */
-router.get('/:id/edit', (req, res, next) => {
-  res.send('EDIT /posts/:id/edit');
-});
+router.get('/:id/edit', asyncErrorHandler(postEdit));
 
 /*  PUT posts UPDTE /posts/:id  */    
-router.put('/:id', (req, res, next) => {
-  res.send('UPDATE /posts/:id ');
-});
+router.put('/:id', asyncErrorHandler(postUpdate));
 
 /*  Delete posts destroy /posts/:id  */
-router.delete('/:id', (req, res, next) => {
-  res.send('DELETE /posts/:id');
-});
+router.delete('/:id', asyncErrorHandler(postDestroy));
 
 module.exports = router;
